e9ca481948ae329ea3ef0b702e86647f70e657de
HARINI S C <113222448+HARINI-S-C@users.noreply.github.com>
2023-10-07 21:37:55 +0530

    Add files via upload

---
+++ SumOfBigInteger.java
@@ -0,0 +1,25 @@
+/*Take two integers as input denoting two large integers and print their sum using BigIntegers.
+The Inputs:
+34
+20
+
+The Output:
+54
+*/
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.math.BigInteger;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		BigInteger A,B,C;
+		A=s.nextBigInteger();
+		B=s.nextBigInteger();
+		C=A.add(B);
+		System.out.println(C);
+	}
+}
