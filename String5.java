1bbeb2d3c4e86f3daf3648fe57f6a15d757e3295
HARINI S C <113222448+HARINI-S-C@users.noreply.github.com>
2023-11-03 13:32:48 +0530

    Add files via upload

---
+++ String5.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Java Program for Calculating ASCII value of a character
+
+Input:
+l 
+
+Output:
+108
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		char ch=s.next().charAt(0);
+		int n=ch;
+		System.out.println(n);
+	}
+}
