63c1142a3e46cb924489cacf122eb4a926250d1e
HARINI S C <113222448+HARINI-S-C@users.noreply.github.com>
2024-02-19 19:46:57 +0530

    Add files via upload

---
+++ CountOfDivisor.java
@@ -0,0 +1,14 @@
+class CountOfDivisor{
+	public static void main(String[] args){
+		int n=15;
+		int a=1;
+		int count=0;
+		while(a<=n/2){
+			if(n%a==0){
+				count++;
+			}
+			a++;
+		}
+		System.out.println(count);
+	}
+}
