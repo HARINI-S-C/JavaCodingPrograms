acdb4f039fb2e855f65ecef5baa07f5b558ddf74
HARINI S C <113222448+HARINI-S-C@users.noreply.github.com>
2024-02-19 19:46:31 +0530

    Add files via upload

---
+++ BasePower.java
@@ -0,0 +1,12 @@
+class BasePower{
+	public static void main(String[] args){
+		int base=2;
+		int power=5;
+		int result=1;
+		while(power>0){
+			result=result*base;
+			power--;
+		}
+		System.out.println(result);
+	}
+}
